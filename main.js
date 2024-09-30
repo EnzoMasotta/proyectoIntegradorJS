1af1c213eb5c690860727c17f7c2a1705dc468d9
EnzoMasotta <en.masotta20@hotmail.com>
2024-09-30 19:48:03 -0300

    Corrigiendo errores

--- main.js
+++ main.js
@@ -6,17 +6,5 @@ const advice = document.querySelector(".advice")
 
 toggleMenu.addEventListener("click", () => {
     navList.classList.toggle("visible");
-    body.classList.toggle("no-scroll");
+    body.classList.toggle('no-scroll');
 })
-
-document.addEventListener("click", (event) => {
-    const isClickInsideMenu = navList.contains(event.target);
-    const isClickOnToggleButton = toggleMenu.contains(event.target);
-    const isClickInsideHeader = header.contains(event.target);
-    const isClickInsideAdivce = advice.contains(event.target); 
-
-    if (!isClickInsideMenu && !isClickOnToggleButton && !isClickInsideHeader && !isClickInsideAdivce && navList.classList.contains("visible")) {
-        navList.classList.remove("visible");
-        body.classList.remove("no-scroll");
-    }
-});
