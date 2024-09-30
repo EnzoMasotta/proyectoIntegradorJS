5ecad098e64f86b0f3f7b297aa0919433ace0872
EnzoMasotta <en.masotta20@hotmail.com>
2024-09-30 14:08:49 -0300

    Pruebas

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const toggleMenu = document.querySelector("#toggle-menu");
 const navList = document.querySelector("#nav-list")
 const body = document.body;
+
 toggleMenu.addEventListener("click", () => {
-    navList.classList.toggle("no-visible");
+    navList.classList.toggle("visible");
     body.classList.toggle("no-scroll");
 })
