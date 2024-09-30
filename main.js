20708c5b13b72b769f89cf1e836f900a867ee19b
EnzoMasotta <en.masotta20@hotmail.com>
2024-09-30 14:50:59 -0300

    Header responsivo V2

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 const toggleMenu = document.querySelector("#toggle-menu");
 const navList = document.querySelector("#nav-list")
 const body = document.body;
+const header = document.querySelector(".nav-bar")
+const advice = document.querySelector(".advice")
 
 toggleMenu.addEventListener("click", () => {
     navList.classList.toggle("visible");
     body.classList.toggle("no-scroll");
 })
+
+document.addEventListener("click", (event) => {
+    const isClickInsideMenu = navList.contains(event.target);
+    const isClickOnToggleButton = toggleMenu.contains(event.target);
+    const isClickInsideHeader = header.contains(event.target);
+    const isClickInsideAdivce = advice.contains(event.target); 
+
+    if (!isClickInsideMenu && !isClickOnToggleButton && !isClickInsideHeader && !isClickInsideAdivce && navList.classList.contains("visible")) {
+        navList.classList.remove("visible");
+        body.classList.remove("no-scroll");
+    }
+});
