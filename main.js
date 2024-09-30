5472c7a54f1c5c896ec83edb6f58e362cf042c3e
EnzoMasotta <en.masotta20@hotmail.com>
2024-09-30 13:33:12 -0300

    Header responsivo

---
+++ main.js
@@ -0,0 +1,7 @@
+const toggleMenu = document.querySelector("#toggle-menu");
+const navList = document.querySelector("#nav-list")
+const body = document.body;
+toggleMenu.addEventListener("click", () => {
+    navList.classList.toggle("no-visible");
+    body.classList.toggle("no-scroll");
+})
