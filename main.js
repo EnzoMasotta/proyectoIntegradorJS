bef167412f3f57c829e8f864f0dd5c9416d703ed
EnzoMasotta <en.masotta20@hotmail.com>
2024-10-06 22:13:35 -0300

    Arreglos carrito y menu

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Menu responsive
 const toggleMenu = document.querySelector("#toggle-menu");
 const navList = document.querySelector("#nav-list")
 const body = document.body;
@@ -7,18 +8,54 @@ const advice = document.querySelector(".advice")
 toggleMenu.addEventListener("click", () => {
     navList.classList.toggle("visible");
     body.classList.toggle('no-scroll');
+
+    if (cart.classList.contains("visibleCart")) {
+        cart.classList.remove("visibleCart");
+        body.classList.remove('no-scroll');
+    }
 })
 
+//Carrito
 const cart = document.querySelector("#cart");
 const openCart = document.querySelector("#open-cart");
 const closeCart = document.querySelector("#close-cart")
 
-openCart.addEventListener("click", () => {
-    cart.classList.add("visibleCart");
-    body.classList.add('no-scroll');
-})
+function lockScroll() {
+    
+    const scrollBarWidth = window.innerWidth - document.documentElement.clientWidth;
+    body.style.paddingRight = `${scrollBarWidth}px`;
+    body.style.overflow = 'hidden';
+}
 
+// Funcion para desbloquear el scroll y restaurar el margen
+function unlockScroll() {
+    body.style.paddingRight = '';
+    body.style.overflow = '';
+}
+
+// Abrir el carrito
+openCart.addEventListener("click", () => {
+    event.stopPropagation();
+    cart.classList.add("visibleCart");
+    lockScroll(); 
+
+    if (navList.classList.contains("visible")) {
+        navList.classList.remove("visible");
+        body.classList.remove('no-scroll');
+    }
+});
+
+// Cerrar el carrito
 closeCart.addEventListener("click", () => {
     cart.classList.remove("visibleCart");
-    body.classList.remove('no-scroll');
-})
+    unlockScroll();  
+});
+
+body.addEventListener("click", (event) => {
+    // Verifica si el clic fue fuera del carrito y no en el botón de abrir
+    if (cart.classList.contains("visibleCart") && !cart.contains(event.target) && event.target !== openCart) {
+        cart.classList.remove("visibleCart");
+        unlockScroll(); // Desbloquea el scroll
+    }
+});
+
